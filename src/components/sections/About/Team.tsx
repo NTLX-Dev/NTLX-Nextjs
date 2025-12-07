589241fabc377ca767fc28005744935d27ae61f8
NexoraTLX-Dev <techNTLX@gmail.com>
2025-12-07 22:42:03 +0530

    Update Team.tsx

--- src/components/sections/About/Team.tsx
+++ src/components/sections/About/Team.tsx
@@ -10,11 +10,11 @@ const Team = () => {
 
         <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-4 gap-y-12 lg:gap-x-16 xl:gap-x-20">
             <div>
-                <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-1.png" alt="" />
+                <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://raw.githubusercontent.com/NTLX-Dev/NTLX-Nextjs/refs/heads/main/public/kanikaNexoraTLX.jpg" alt="" />
                 <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Kanika Agarwal</p>
                 <p className="mt-2 text-base font-normal text-gray-600 font-pj">Founder, Chairman, Executive Director</p>
             </div>
-
+            {/*
             <div>
                 <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-2.png" alt="" />
                 <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Jerome Bell</p>
@@ -33,7 +33,7 @@ const Team = () => {
                 <p className="mt-2 text-base font-normal text-gray-600 font-pj">Co-founder, Chairman, Executive Director</p>
             </div>
         </div>
-
+        */}
         <div className="mt-12 sm:mt-16">
             <svg className="w-auto h-4 mx-auto text-gray-300" viewBox="0 0 172 16" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 11 1)" />
