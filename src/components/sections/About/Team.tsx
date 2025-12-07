ae98669e164c0485f498ac9951153a97110848c5
NexoraTLX-Dev <techNTLX@gmail.com>
2025-12-07 23:09:47 +0530

    Update Team.tsx

--- src/components/sections/About/Team.tsx
+++ src/components/sections/About/Team.tsx
@@ -8,7 +8,8 @@ const Team = () => {
             <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">Our{" "}<span className='text-red-600'>Investors</span>{" "}& Board of{" "}<span className='text-red-600'>Directors</span></h2>
         </div>
 
-        <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-4 gap-y-12 lg:gap-x-16 xl:gap-x-20">
+        <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-3 gap-y-12 lg:gap-x-16 xl:gap-x-20">
+            <div></div>
             <div>
                 <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://raw.githubusercontent.com/NTLX-Dev/NTLX-Nextjs/refs/heads/main/public/kanikaNexoraTLX.jpg" alt="" />
                 <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Kanika Agarwal</p>
